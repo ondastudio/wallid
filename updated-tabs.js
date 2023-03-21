df2546e40fb69750cc40a1fc59ffa36fd2407866
Onda <75018767+ondastudio@users.noreply.github.com>
2023-03-21 17:11:01 +0000

    Update updated-tabs.js

--- updated-tabs.js
+++ updated-tabs.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function() {
   $("#tabs").on("inview", function (event, isInView) {
     if (isInView) {
       // Select the SVG element
