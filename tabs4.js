08f22907748070b636069061ba15450ee7adb17f
Onda <75018767+ondastudio@users.noreply.github.com>
2023-03-22 14:27:47 +0000

    Update tabs4.js

--- tabs4.js
+++ tabs4.js
@@ -1,53 +1,55 @@
-  $("#tabs").on("inview", function (event, isInView) {
-  if (isInView) {
+    document.addEventListener("DOMContentLoaded", function () {
+
+    $("#tabs").on("inview", function (event, isInView) {
+    if (isInView) {
+      // Select the SVG element
+      const svgElement = document.querySelector(".svg-tab1");
+      // Define the DrawSVG timeline
+      const drawSVGTimeline = gsap.timeline();
+      // Set up the DrawSVG animation
+      drawSVGTimeline.to(svgElement, {
+        defaults: { ease: "none" }
+      });
+      //draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
+      gsap.staggerFrom(".draw-me", 1, { drawSVG: 0 }, 0.5);
+      gsap.staggerFrom(".draw-me-mobile", 1, { drawSVG: 0 }, 0.5);
+      // Start the animation
+      drawSVGTimeline.play();
+    }
+    });
+
+  $(".trigger-2").on("click", function () {
+    //tab2
+
     // Select the SVG element
-    const svgElement = document.querySelector(".svg-tab1");
+    const svgElement2 = document.querySelector(".svg-tab2");
     // Define the DrawSVG timeline
-    const drawSVGTimeline = new TimelineLite({
+    const drawSVGTimeline2 = gsap.timeline();
+    // Set up the DrawSVG animation
+    drawSVGTimeline2.to(svgElement2, {
       defaults: { ease: "none" }
     });
-    // Set up the DrawSVG animation
-    drawSVGTimeline.to(svgElement);
-    // Draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
-    drawSVGTimeline.staggerFrom(".draw-me", 1, { drawSVG: 0 }, 0.5);
-    drawSVGTimeline.staggerFrom(".draw-me-mobile", 1, { drawSVG: 0 }, 0.5);
+    //draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
+    TweenMax.staggerFrom(".draw-me2", 3, { drawSVG: 0 }, 0.5);
+    TweenMax.staggerFrom(".draw-me-mobile2", 3, { drawSVG: 0 }, 0.5);
     // Start the animation
-    drawSVGTimeline.play();
-  }
-});
-
-$(".trigger-2").on("click", function () {
-  // Tab 2
-
-  // Select the SVG element
-  const svgElement2 = document.querySelector(".svg-tab2");
-  // Define the DrawSVG timeline
-  const drawSVGTimeline2 = gsap.timeline({
-    defaults: { ease: "none" }
+    drawSVGTimeline2.play();
   });
-  // Set up the DrawSVG animation
-  drawSVGTimeline2.to(svgElement2);
-  // Draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
-  drawSVGTimeline2.staggerFrom(".draw-me2", 3, { drawSVG: 0 }, 0.5);
-  drawSVGTimeline2.staggerFrom(".draw-me-mobile2", 3, { drawSVG: 0 }, 0.5);
-  // Start the animation
-  drawSVGTimeline2.play();
-});
 
-$(".trigger-3").on("click", function () {
-  // Tab 3
+  $(".trigger-3").on("click", function () {
+    //tab3
 
-  // Select the SVG element
-  const svgElement3 = document.querySelector(".svg-tab3");
-  // Define the DrawSVG timeline
-  const drawSVGTimeline3 = gsap.timeline({
-    defaults: { ease: "none" }
+    // Select the SVG element
+    const svgElement3 = document.querySelector(".svg-tab3");
+    // Define the DrawSVG timeline
+    const drawSVGTimeline3 = gsap.timeline();
+    // Set up the DrawSVG animation
+    drawSVGTimeline3.to(svgElement3, {
+      defaults: { ease: "none" }
+    });
+    //draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
+    TweenMax.staggerFrom(".draw-me3", 3, { drawSVG: 0 }, 0.5);
+    TweenMax.staggerFrom(".draw-me-mobile3", 3, { drawSVG: 0 }, 0.5);
+    // Start the animation
+    drawSVGTimeline3.play();
   });
-  // Set up the DrawSVG animation
-  drawSVGTimeline3.to(svgElement3);
-  // Draws all elements with the "draw-me" class applied with staggered start times 0.5 seconds apart
-  drawSVGTimeline3.staggerFrom(".draw-me3", 3, { drawSVG: 0 }, 0.5);
-  drawSVGTimeline3.staggerFrom(".draw-me-mobile3", 3, { drawSVG: 0 }, 0.5);
-  // Start the animation
-  drawSVGTimeline3.play();
-});
